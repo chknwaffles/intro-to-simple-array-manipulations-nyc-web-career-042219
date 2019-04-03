bb76467c0943b046f8b0941b8957364bce4916ad
Won Kim <wonsae.kim@gmail.com>
2019-04-03 21:09:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,7 +1,7 @@
-def using_push(array, string)
-  array.push(string)
+def using_push(arr, str)
+  arr.push(str)
 end
 
-def using_unshift(arr, string)
-  arr.unshift(string)
+def using_unshift(arr, str)
+  arr.unshift(str)
 end
